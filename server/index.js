2f664893aba49bd44950a05e9d90afc82093efb9
nexucipherguard <nexuscipherguard@gmail.com>
2026-01-25 17:33:05 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://securecomm.netlify.app"],
+    origin: ["http://localhost:5173", "https://securecomm1.netlify.app"],
     methods: ["GET", "POST"]
   }
 });
